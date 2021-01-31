882c1576af915d9ebf75330b27042352205d12bf
Artem Sivyankov <morfiy0393@gmail.com>
2021-01-31 15:36:17 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
 exports.min = function min (array) {
-  return 0;
-}
+  if (array ==  null || array.length == 0) {
+    return 0;
+  }
+  return Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    return Math.avg(...array);
+};
