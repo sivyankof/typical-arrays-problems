7874d8c540b8f9c9d6b2873b4326f81ca17c39a4
Artem Sivyankov <morfiy0393@gmail.com>
2021-01-31 15:50:50 +0300

    fix: edit avg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-exports.min = function min (array) {
-  if (array ==  null || array.length == 0) {
-    return 0;
-  }
-  return Math.min(...array);
+exports.min = function min(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    return Math.min(...array);
 };
 
 exports.max = function max(array) {
@@ -16,5 +16,10 @@ exports.avg = function avg(array) {
     if (array == null || array.length == 0) {
         return 0;
     }
-    return Math.avg(...array);
+    let sum = [...array].reduce((a, b) => a + b, 0);
+    let result = sum / array.length;
+    console.log(result);
+    return result.toFixed(2);
 };
+
+
